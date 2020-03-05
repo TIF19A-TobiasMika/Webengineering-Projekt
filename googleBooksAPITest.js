226d620695f0b1763b3971f1b33787a936145da2
Mika Busch <haeki@hotmail.de>
2020-03-05 10:51:25 +0100

    Thumbnails working

--- googleBooksAPITest.js
+++ googleBooksAPITest.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('run').onclick = function(){
       const req = new XMLHttpRequest();
-      req.open("GET",'https://www.googleapis.com/books/v1/volumes?q=Harray+Potter&key=AIzaSyCg0v6ii17dHIn0ZfQMIfMD0qshWRuFio0', true);
+      let searchText = document.getElementById("searchText").value;
+      searchText = searchText.replace(/ /g, '+');
+      //console.log(searchText);
+      console.log('https://www.googleapis.com/books/v1/volumes?q=' + searchText + '&key=AIzaSyCg0v6ii17dHIn0ZfQMIfMD0qshWRuFio0');
+      req.open("GET",'https://www.googleapis.com/books/v1/volumes?q=' + searchText + '&key=AIzaSyCg0v6ii17dHIn0ZfQMIfMD0qshWRuFio0', true);
       req.send();
       req.onload=function(){
-        const json = JSON.parse(req.responseText);
-        document.getElementById('json').innerHTML = JSON.stringify(json);
-        //let output = json.items[1].volumeInfo.title;
-        let html = "";
+        let json = JSON.parse(req.responseText);
+        let fullJson = JSON.stringify(json);
+        let output = document.getElementById('output');
+        output.innerHTML = "";
         json.items.forEach(function(val) {
-          html += "<p>" + JSON.stringify(val.volumeInfo.title) + "</p>"
+          let image = document.createElement("IMG");
+          image.setAttribute("src", val.volumeInfo.imageLinks.thumbnail);
+          image.setAttribute("alt", val.volumeInfo.title);
+          output.appendChild(image);
         });
-        document.getElementById('output').innerHTML = html;
         console.log(output);
       };
     };
