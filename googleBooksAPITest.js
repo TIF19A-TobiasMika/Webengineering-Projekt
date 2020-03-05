d2f29b46eb7523cd2d92325b8ff57726c9c14399
Mika Busch <haeki@hotmail.de>
2020-03-05 12:47:10 +0100

    Switched to fetch

--- googleBooksAPITest.js
+++ googleBooksAPITest.js
@@ -1,24 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('run').onclick = function(){
-      const req = new XMLHttpRequest();
+    document.getElementById('run').onclick = async function(){
       let searchText = document.getElementById("searchText").value;
       searchText = searchText.replace(/ /g, '+');
-      //console.log(searchText);
-      console.log('https://www.googleapis.com/books/v1/volumes?q=' + searchText + '&key=AIzaSyCg0v6ii17dHIn0ZfQMIfMD0qshWRuFio0');
-      req.open("GET",'https://www.googleapis.com/books/v1/volumes?q=' + searchText + '&key=AIzaSyCg0v6ii17dHIn0ZfQMIfMD0qshWRuFio0', true);
-      req.send();
-      req.onload=function(){
-        let json = JSON.parse(req.responseText);
-        let fullJson = JSON.stringify(json);
-        let output = document.getElementById('output');
-        output.innerHTML = "";
-        json.items.forEach(function(val) {
-          let image = document.createElement("IMG");
-          image.setAttribute("src", val.volumeInfo.imageLinks.thumbnail);
-          image.setAttribute("alt", val.volumeInfo.title);
-          output.appendChild(image);
-        });
-        console.log(output);
-      };
+      getImages(searchText);
     };
   });
+
+  async function getImages(searchText) {
+      const fetchRequest = `https://www.googleapis.com/books/v1/volumes?q=${searchText}&printType=books&maxResults=30&projection=lite&key=AIzaSyCg0v6ii17dHIn0ZfQMIfMD0qshWRuFio0`;
+      console.log(fetchRequest);
+      const fetchResult = await fetch(fetchRequest);
+      let json = await fetchResult.json();
+      let fullJson = JSON.stringify(json);
+      //console.log(fullJson);
+      let output = document.getElementById('output');
+      output.innerHTML = "";
+      json.items.forEach(function(val) {
+        let image = document.createElement("IMG");
+        image.setAttribute("src", val.volumeInfo.imageLinks.thumbnail);
+        image.setAttribute("alt", val.volumeInfo.title);
+        output.appendChild(image);
+      });
+      //console.log(output);
+  }
